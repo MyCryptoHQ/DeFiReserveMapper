eaeff4f00c07d1831fa2bcf0ab96c46190a0dfc7
Mia <mia.devops@gmail.com>
2020-03-14 14:33:46 -0700

    Terraform fmt

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "defi_s3_bucket" { // creates a new `aws_s3_bucket` resource and gives it an id `defi_s3_bucket`
-  bucket = "defi-reserve-mapper" // names the bucket `defi-reserve-mapper`
-  acl    = "private" // defines the bucket as private
+  bucket = "defi-reserve-mapper"            // names the bucket `defi-reserve-mapper`
+  acl    = "private"                        // defines the bucket as private
 
   versioning {
     enabled = true // enables versioning for the `defi_s3_bucket` resource
@@ -8,6 +8,6 @@ resource "aws_s3_bucket" "defi_s3_bucket" { // creates a new `aws_s3_bucket` res
 
   tags = {
     Name        = "defi-reserve-mapper" // sets bucket tag in aws to "defi-reserve-mapper"
-    Environment = "defi-reserve-mapper"   // sets environment that the `defi-reserve-mapper` bucket belongs to
+    Environment = "defi-reserve-mapper" // sets environment that the `defi-reserve-mapper` bucket belongs to
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
-  type        = string
+  type        = "string"
   description = "The region to operate out of."
   default     = "us-east-1"
 }
 
 variable "profile" {
-  type = string
+  type        = "string"
   description = "Name of AWS credentials profile."
-  default = "default"
+  default     = "default"
 }
