a68d373d8aad2c28a5d3448a1879f7c4fc82619a
blurpesec <hahn.michael.f@gmail.com>
2020-03-15 16:55:52 -0700

    fix tf fmt

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "defi_s3_bucket" { // creates a new `aws_s3_bucket` resource and gives it an id `defi_s3_bucket`
-  bucket = var.bucket                       // names the bucket `defi-reserve-mapper`
-  acl    = "private"                        // defines the bucket as private
+  bucket        = var.bucket                // names the bucket `defi-reserve-mapper`
+  acl           = "private"                 // defines the bucket as private
   force_destroy = true
 
   versioning {
